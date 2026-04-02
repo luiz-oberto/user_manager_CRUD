06265bd0374c0aaccfc17d72bc1270f85a773334
Luiz Oberto <lom.raiol@gmail.com>
2026-04-02 20:36:39 -0300

    add middleware - CORS

--- app/main.py
+++ app/main.py
@@ -5,12 +5,27 @@ from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
 from app.routes.auth_routes import router as auth_router
 from app.utils.init_admin import create_initial_admin
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 
 app = FastAPI(
     title="User Manager API",
     version="1.0.0"
+)
+
+# Domínios permitidos (ajuste conforme necessário)
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # lista de domínios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],    # permite GET, POST, PUT, DELETE, etc
+    allow_headers=["*"],    # permite todos headers (Authorization incluído)
 )
 
 logger = logging.getLogger(__name__)
